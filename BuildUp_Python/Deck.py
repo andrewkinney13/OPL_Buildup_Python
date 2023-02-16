b8804feedbca4697c43466025655d5656a1da308
Andrew Kinney <andrewkinney13@gmail.com>
2023-02-15 22:11:12 -0500

    Tile class done

--- BuildUp_Python/Deck.py
+++ BuildUp_Python/Deck.py
@@ -3,12 +3,14 @@ from Tile import Tile
 
 class Deck:
 
+    # Constructor
     def __init__(self, tileSetSize):
         self.boneyard = []
         self.stack = []
         self.hand = []
+        self.maxTilePips = tileSetSize
 
-    pass
+    
 
 
 
--- BuildUp_Python/Tile.py
+++ BuildUp_Python/Tile.py
@@ -1,6 +1,46 @@
 
 class Tile:
-    pass
+
+    # Constructor 
+    def __init__(self, color, sideOnePips, sideTwoPips):
+        self.color = color
+        self.sideOnePips = sideOnePips
+        self.sideTwoPips = sideTwoPips
+
+    # Returns if tile is a doulbe
+    def IsDouble(self):
+        if (self.sideOnePips == self.sideTwoPips):
+            return True
+        else:
+            return False
+
+    # Returns total pip value of the tile
+    def GetValue(self):
+        return self.sideOnePips + self.sideTwoPips
+
+    # Overloaded operators
+    def __gt__(self, other):
+
+        # This tile is non-double
+        if (not self.IsDouble()):
+
+            # Compare values
+            if (self.GetValue() > other.GetValue()):
+                return True
+
+            else:
+                return False
+
+        # This tile is a double
+        else:
+
+            # The other tile is a non-double
+            if (not other.IsDouble()):
+                return True
+
+            # They're both doubles
+            else:
+                return self.sideOnePips > other.sideOnePips
 
 
 
